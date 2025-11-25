b79a1731700764c337ff89271cd01b470f54d94b
bhavs09 <76781021+bhavs09@users.noreply.github.com>
2025-11-25 10:39:17 +0530

    Refactor Docker login and push command

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,9 @@
 def call(String Project, String ImageTag, String dockerUserFromParam){
-  withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]){
-    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-  }
-  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+    withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+        // Secure login
+        sh """
+            echo "$dockerHubPass" | docker login -u "$dockerHubUser" --password-stdin
+            docker push "$dockerHubUser/$Project:$ImageTag"
+        """
+    }
 }
